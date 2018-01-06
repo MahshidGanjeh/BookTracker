aeb8f52e199327060b49d3aa55350bd2b4b676b9
Hp <mahshidga@gmail.com>
2018-01-07 01:30:17 +0330

    Creating Book Item for the list

--- app/src/main/java/com/example/booktracker/MainActivity.java
+++ app/src/main/java/com/example/booktracker/MainActivity.java
@@ -4,16 +4,44 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.example.booktracker.Networking.VolleySingleton;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String URL = "https://www.goodreads.com/search.xml" +
+            "?key=GWMAmEgkrVLUHvbMS1oTpQ&q=City%27s";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Fetching data using Volley
+        StringRequest strReq = new StringRequest(URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Response is", response.toString());
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error is", "Error: " + error.getMessage());
+            }
+        });
+        // Adding String request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).
+                addToRequestQueue(strReq, "dd");
+
+        //Setting Up Bottom Navigation
         BottomNavigationView bottomNavigationView =
                 (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
---
+++ app/src/main/java/com/example/booktracker/Networking/VolleySingleton.java
@@ -0,0 +1,73 @@
+package com.example.booktracker.Networking;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Hp on 1/6/2018.
+ */
+
+public class VolleySingleton {
+
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static VolleySingleton mVolleySingletonInstance;
+    private static Context mContext;
+
+    private VolleySingleton(Context context) {
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap>
+                    cache = new LruCache<String, Bitmap>(20);
+
+            @Override
+            public Bitmap getBitmap(String url) {
+                return cache.get(url);
+            }
+
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                cache.put(url, bitmap);
+            }
+        });
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mVolleySingletonInstance == null) {
+            mVolleySingletonInstance = new VolleySingleton(context);
+        }
+        return mVolleySingletonInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(tag);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}

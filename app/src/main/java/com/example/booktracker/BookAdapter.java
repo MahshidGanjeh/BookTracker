6c56dbe9fdfe417fa8868c83010173136bb40ded
Hp <mahshidga@gmail.com>
2018-01-09 01:18:57 +0330

    Creating RecyclerView

---
+++ app/src/main/java/com/example/booktracker/BookAdapter.java
@@ -0,0 +1,8 @@
+package com.example.booktracker;
+
+/**
+ * Created by Hp on 1/8/2018.
+ */
+
+public class BookAdapter {
+}
---
+++ app/src/main/java/com/example/booktracker/Networking/Book.java
@@ -0,0 +1,8 @@
+package com.example.booktracker.Networking;
+
+/**
+ * Created by Hp on 1/8/2018.
+ */
+
+public class Book {
+}
